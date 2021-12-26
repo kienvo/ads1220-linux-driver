63c448b224fc447a40e06d6e5a1055ad0eab8f9d
kienvo <kienvo@gihub.com>
2021-12-26 15:19:25 +0700

    Net stream number to client

--- user-app/read.cpp
+++ user-app/read.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
+
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <netdb.h>
 #include <netinet/in.h>
@@ -17,50 +19,92 @@ static void loading(void)
 		<< std::flush;
 }
 
-
-
-int net_init() 
+class net
 {
-	struct sockaddr_in server_addr = {}, client_addr = {};
-	int portnum = 12345;
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	char buf[100] = {};
-	
+private:
+	int _sockfd, _port, accepted_client;
+	struct sockaddr_in _server_addr = {}, _client_addr = {};
+	unsigned int client_addr_len = sizeof(sockaddr_in);
+public:
+	net(int port);
+	~net();
+	int Listener() 
+	{
+		listen(_sockfd, 1);
+		accepted_client = 
+			accept(_sockfd, (struct sockaddr*)&_client_addr, &client_addr_len);
+		if(accepted_client < 0) {
+			perror("accept() error");
+			exit(1);
+		}
+		return accepted_client;
+	}
+	int Read(char *buf, int count) 
+	{
+		int nread = read(accepted_client, buf, count);
+		if(nread < 0) {
+			perror("read error");
+			exit(1);
+		}
+		return nread;
+	}
+	int Write(char const *buf, int count)
+	{
+		int nwrite = write(accepted_client, buf, count);
+		if(write < 0) {
+			perror("write error");
+			exit(1);
+		}
+		return nwrite;
+	}
+	int WriteAsChar(int32_t num) 
+	{
+		auto s = std::to_string(num);
+		s += '\n';	// TODO: temporarily let it here
+		return Write(s.c_str(), s.length());
+		
+	}
+	inline void Shutdown() 
+	{
+		shutdown(accepted_client, SHUT_RDWR);
+	}
+};
 
-	server_addr.sin_addr.s_addr	= INADDR_ANY;
-	server_addr.sin_family 		= AF_INET;
-	server_addr.sin_port 		= htons(portnum);
-	if(sockfd < 0) {
+net::net(int port):
+_port(port)
+{
+
+	_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(_sockfd < 0) {
 		perror("Cannot open socket");
 		exit(-1);
 	}
-	if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr))) {
+	_server_addr.sin_addr.s_addr	= INADDR_ANY;
+	_server_addr.sin_family 		= AF_INET;
+	_server_addr.sin_port 		= htons(port);
+	if (bind(_sockfd, (struct sockaddr*)&_server_addr, sizeof(_server_addr))) {
 		perror("bind() error");
 		exit(1);
 	}
-	listen(sockfd, 1);
-	unsigned int client_addr_len = sizeof(client_addr);
-	int client_accepted = 
-	accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_len);
-	if(client_accepted < 0) {
-		perror("accept() error");
-		exit(1);
-	}
-	int nread = read(client_accepted, buf, 100);
-	if(nread < 0) {
-		perror("read error");
-		exit(1);
-	}
-	std::cout << "The message: " << buf << std::endl;
-	const char *mess = "gotcha";
-	int nwrite = write(client_accepted, mess, strlen(mess));
-	if(write < 0) {
-		perror("write error");
-		exit(1);
-	}
-	shutdown(client_accepted, SHUT_RDWR);
-	close(sockfd);
-	return sockfd;
+}
+
+net::~net()
+{
+	Shutdown();
+	close(_sockfd);
+}
+
+void net_test() 
+{
+	net test(12345);
+	test.Listener();
+	char buf[200]={};
+	test.Read(buf, 200);
+	std::cerr << "Message is: " << buf << std::endl;
+
+	char *str = "Hello from Host\n";
+	test.Write(str, strlen(str));
+	std::cerr << "Message from Host has been sent" << std::endl;
 }
 
 bool is_stdout_redirected() 
@@ -73,9 +117,13 @@ int main()
 {
 	int32_t data;
 	ssize_t ret;
+
+	net *toPloter = new net(12345);
+	toPloter->Listener();
+
 	int fd = open("/dev/ads1220", O_RDONLY|O_NONBLOCK);
 	std::cerr << "\nReadding... " << std::flush;
-	net_init();
+
 	if(fd<0) {
 		perror("Module was not loaded");
 		return -1;
@@ -89,9 +137,11 @@ int main()
 			return -1;
 		}
 		std::cout << data << std::endl;
+		toPloter->WriteAsChar(data);
 		if(redirected) loading();
 	}
-	close(fd);
 	std::cerr << std::endl;
+	close(fd);
+	delete(toPloter);
 	return 0;
 }
